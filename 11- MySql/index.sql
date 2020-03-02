be61b003bc34009636daf1a374f4b919d8514df6
Jonathan Souza <jonathanbcsouza@hotmail.com>
2020-03-03 00:56:14 +1300

    cleaning up

--- 11- MySql/index.sql
+++ 11- MySql/index.sql
@@ -1,18 +1,20 @@
 /*
 
- - MYSQL INSTALLATION PROCESS:
+- MYSQL INSTALLATION PROCESS:
 
- In your terminal:
- mysql: To check SQL.
- brew search mysql: To check your SQL if you used brew to install PHP.
- brew install mysql: You may need to do to fetch that.
- mysql -uroot: to login into the database
- mysql -uroot: to login into the database if you have a password. */
+        In your terminal:
+        
+        mysql: To check SQL.
+        brew search mysql: To check your SQL if you used brew to install PHP.
+        brew install mysql: You may need to do to fetch that.
+        mysql -uroot: to login into the database
+        mysql -uroot: to login into the database if you have a password.
 
--- SQL COMMANDS;
+- SQL COMMANDS;
+ */
 
 show databases;
-/* Output
+        /* Output
         +--------------------+
         | Database           |
         +--------------------+
@@ -26,8 +28,9 @@ show databases;
 create database myTodo;
 use myTodo;
 create table todos (description text, completed boolean);
+
 show tables;
-/* Output
+        /* Output
         +------------------+
         | Tables_in_mytodo |
         +------------------+
@@ -36,7 +39,7 @@ show tables;
         1 row in set (0.01 sec) */
 
 describe todos;
-/* Output
+        /* Output
         +-------------+------------+------+-----+---------+-------+
         | Field       | Type       | Null | Key | Default | Extra |
         +-------------+------------+------+-----+---------+-------+
@@ -44,17 +47,17 @@ describe todos;
         | completed   | tinyint(1) | YES  |     | NULL    |       |
         +-------------+------------+------+-----+---------+-------+
         2 rows in set (0.01 sec) */
+
 drop table todos;
-/* Output
+        /* Output
         Query OK, 0 rows affected (0.01 sec) */
 
-create table todos
-(description text NOT NULL, completed boolean NOT NULL);
-/* Output
+create table todos (description text NOT NULL, completed boolean NOT NULL);
+        /* Output
         Query OK, 0 rows affected (0.01 sec) */
 
 describe todos;
-/* Output
+        /* Output
         +-------------+------------+------+-----+---------+-------+
         | Field       | Type       | Null | Key | Default | Extra |
         +-------------+------------+------+-----+---------+-------+
@@ -64,11 +67,11 @@ describe todos;
         2 rows in set (0.00 sec) */
 
 create table todos (id integer PRIMARY KEY AUTO_INCREMENT, description text NOT NULL, completed boolean NOT NULL);
-/* Output
+        /* Output
         Query OK, 0 rows affected (0.01 sec) */
 
 show tables;    
-/* Output
+        /* Output
         +------------------+
         | Tables_in_mytodo |
         +------------------+
@@ -77,7 +80,7 @@ show tables;
         1 row in set (0.00 sec) */
 
 describe todos;
-/* Output
+        /* Output
         +-------------+------------+------+-----+---------+----------------+
         | Field       | Type       | Null | Key | Default | Extra          |
         +-------------+------------+------+-----+---------+----------------+
@@ -87,19 +90,27 @@ describe todos;
         +-------------+------------+------+-----+---------+----------------+
         3 rows in set (0.00 sec) */
 
-insert into todos
-        (description, completed)
-values('Go to the store', false);
+insert into todos (description, completed) values('Go to the store', false);
+insert into todo (description, completed) values('I am the second one!', false);
 
-insert into todos
-        (description, completed)
-values('I am the second one!', false);
+select * from todos;
 
-/* SUGGESTED MANAGEMENT APPS: 
+        /* Output
+        +----+----------------------+-----------+
+        | id | description          | completed |
+        +----+----------------------+-----------+
+        |  1 | Go to the store      |         0 |
+        |  2 | I am the second one! |         0 |
+        +----+----------------------+-----------+
+        2 rows in set (0.00 sec)
 
-- Sequel Pro - https://www.sequelpro.com/
-- Queries - https://www.araelium.com/querious
-- Navicat - https://www.navicat.com/en/
+/*
 
-Useful article that helped to fix a configuration issue I had::
-https://medium.com/@devontem/solved-cant-connect-to-local-mysql-server-through-socket-tmp-mysql-sock-2-f52c9c546f7 */
+- SUGGESTED MANAGEMENT APPS: 
+
+        - Sequel Pro - https://www.sequelpro.com/
+        - Queries - https://www.araelium.com/querious
+        - Navicat - https://www.navicat.com/en/
+
+        Useful article that helped to fix a configuration issue I had::
+        https://medium.com/@devontem/solved-cant-connect-to-local-mysql-server-through-socket-tmp-mysql-sock-2-f52c9c546f7 */
