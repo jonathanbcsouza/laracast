1b376cc0696f82dc850bfd9b5c82421adaa622c8
Jonathan Souza <jonathanbcsouza@hotmail.com>
2021-11-08 08:28:53 +1300

    Add new example

--- 18- Array Filtering/index.php
+++ 18- Array Filtering/index.php
@@ -19,6 +19,9 @@ $posts = [
     new Post('My Fourth Post', true)
 ];
 
+// Comment and uncomment the variable and its associated var_dump if you wish to see in the browser.
+// Example below
+
 $unpublishedPosts = array_filter(
     $posts,
     function ($post) {
@@ -27,24 +30,34 @@ $unpublishedPosts = array_filter(
 );
 var_dump($unpublishedPosts);
 
-$publishedPosts = array_filter(
-    $posts,
-    function ($post) {
-        return $post->published;
-    }
-);
-var_dump($publishedPosts);
+// $publishedPosts = array_filter(
+//     $posts,
+//     function ($post) {
+//         $post;
+//         return $post->published;
+//     }
+// );
+// var_dump($publishedPosts);
 
-$modified = array_map(function ($post) {
-    $post->published = true;
-    return $post;
-}, $posts);
-var_dump($modified);
+// $modified = array_map(function ($post) {
+//     $post->published = true;
+//     return $post;
+// }, $posts);
+// var_dump($modified);
 
-$titles = array_map(function ($post) {
-    return $post->title;
-}, $posts);
-var_dump($titles);
 
-$titlesAgain= array_column($posts, 'title');
-var_dump($titlesAgain);
+// $titles = array_map(function ($post) {
+//     return $post->title;
+// }, $posts);
+// var_dump($titles);
+
+// $titlesAgain = array_column($posts, 'title');
+// var_dump($titlesAgain);
+
+// $isPublished = array_map(function ($post) {
+//     return $post->published;
+// }, $posts);
+// var_dump($isPublished);
+
+// $isPublishedAgain = array_column($posts, 'published');
+// var_dump($isPublishedAgain);
